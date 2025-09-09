1413332e89fb863d95950df34439546bb70b11a2
MUSTAFAREZA47 <mustafareza47@gmail.com>
2025-09-10 00:23:54 +0300

    feat: integrate Convex for backend functionality and update dependencies in web and widget apps

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,20 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
-import { Input } from "@workspace/ui/components/input"
+"use client"
+
+import { useQuery, useMutation } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
+
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
+
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Ahmed - web</h1>
-        <Button size="sm">Button</Button>
-          <p>{add(1, 2)}</p>
-          <Input />
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <h1>Apps - Web</h1>
+      <p>{users?.map((user) => user.name).join(", ")}</p>
+      <Button onClick={() => addUser()}>Add User</Button>
     </div>
   )
 }
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,20 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
+"use client"
+
+import { useQuery, useMutation } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
 
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
+
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Ahmed - widget</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(9, 2)}</p>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <h1>Apps - Widget</h1>
+      <p>{users?.map((user) => user.name).join(", ")}</p>
+      <Button onClick={() => addUser()}>Add User</Button>
     </div>
   )
 }
