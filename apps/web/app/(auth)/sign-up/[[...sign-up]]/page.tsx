235b058551bf9b8ccfde6d15fa2d2fdfa4394686
MUSTAFAREZA47 <mustafareza47@gmail.com>
2025-09-11 00:52:38 +0300

    feat(web): integrate Clerk for authentication, add middleware, and update UI components

---
+++ apps/web/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from "@clerk/nextjs";
+
+const SignInPage = () => {
+  return <div>
+    <h1>Sign In</h1>
+     <SignUp />
+  </div>;
+};
+
+export default SignInPage;
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { useQuery, useMutation } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { useQuery, useMutation, Authenticated, Unauthenticated  } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 
@@ -11,10 +12,24 @@ export default function Page() {
 
 
   return (
+    <>
+    <Authenticated>
     <div className="flex flex-col items-center justify-center min-h-svh">
+      <UserButton />
       <h1>Apps - Web</h1>
       <p>{users?.map((user) => user.name).join(", ")}</p>
       <Button onClick={() => addUser()}>Add User</Button>
     </div>
+    </Authenticated>
+
+    <Unauthenticated>
+      <h1>Please sign in</h1>
+        <SignInButton>
+          Sign In
+        </SignInButton>
+  
+      
+    </Unauthenticated>
+    </>
   )
 }
