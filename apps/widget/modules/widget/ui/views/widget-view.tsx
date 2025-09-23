6d37c5128f02c9815d7f900735f41ef930cc97ab
MUSTAFAREZA47 <mustafareza47@gmail.com>
2025-09-23 11:10:40 +0300

    feat(widget): add authentication screen and integrate form handling with react-hook-form; update dependencies for form validation and state management

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -2,6 +2,10 @@
 
 import { WidgetFooter } from "../components/widget-footer";
 import { WidgetHeader } from "../components/widget-header";
+import { WidgetAuthScreen } from "../screens/widget-auth-screen";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@workspace/ui/components/form"
+import { Button } from "@workspace/ui/components/button"
+
 
 interface Props {
     organizationId: string;
@@ -11,19 +15,8 @@ export const WidgetView = ({ organizationId }: Props) => {
     return (
         // TODO: Correct this to be min-h-svh
         <main className="min-h-svh min-w-svh flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-            <WidgetHeader>
-                <div className="flex flex-col justify-between gap-y-2 px-2 py-6">
-                    <p className="text-3xl">Hey There! 👋</p>
-                    <p className="text-lg">
-                        How can we help you today?
-                    </p>
-                </div>
-            </WidgetHeader>
-            <div className="flex flex-1">
-                <h1>Widget View</h1>
-                <p>{organizationId}</p>
-            </div>
-            <WidgetFooter />
+            <WidgetAuthScreen />
+            {/* <WidgetFooter /> */}
         </main>
     )
 }
